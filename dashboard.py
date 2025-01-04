39f2ed480c1fc9398a6c1d9cc083f2b96240533f
aryo-malik22 <m006b4ky0686@bangkit.academy>
2025-01-04 10:25:51 +0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("For Kasih Riesdiani Azzahara")
+
+st.subheader("Please read this")
+st.write("I'm deeply sorry for making you discomfort because I said something that I shouldn't have said.")
+
+st.subheader("There's more, please read this too :)")
+st.write("I do not want this to become the end of our conversation and I want to keep it on and on, "
+         "we promised each other that we will do every wishlist that we have, so let's do it together as planned.")
+
+st.subheader("HAPPY BIRTHDAY DIANNNN")
+st.write("Happy birthday Kasih Riesdiani Azzahra, may this year become the best year that you'll have.")
+
+if st.button("One more surprise"):
+    st.balloons()
+    st.success("You are amazing! Let's make wonderful memories together!")
